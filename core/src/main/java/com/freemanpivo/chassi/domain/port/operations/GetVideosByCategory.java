a5289ad444f6eb937f8b7bc402e37dcc15b24c43
Pedro Zanon <p.hzanon123@gmail.com>
2021-07-26 21:55:26 -0300

    create search videos by category ports

--- core/src/main/java/com/freemanpivo/chassi/domain/port/operations/GetVideosByCategory.java
+++ core/src/main/java/com/freemanpivo/chassi/domain/port/operations/GetVideosByCategory.java
@@ -1,2 +1,10 @@
-package com.freemanpivo.chassi.domain.port.operations;public interface GetVideosByCategory {
+package com.freemanpivo.chassi.domain.port.operations;
+
+import com.freemanpivo.chassi.domain.model.Video;
+
+import java.util.List;
+
+public interface GetVideosByCategory {
+
+    List<Video> getAllVideosByCategory(Long id);
 }
