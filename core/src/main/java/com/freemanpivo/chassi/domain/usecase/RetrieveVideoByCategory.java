5046f32f9b8a1fced9da7e979a03d316610a276b
Pedro Zanon <p.hzanon123@gmail.com>
2021-07-26 21:57:05 -0300

    create retrieve videos by category usecase

--- core/src/main/java/com/freemanpivo/chassi/domain/usecase/RetrieveVideoByCategory.java
+++ core/src/main/java/com/freemanpivo/chassi/domain/usecase/RetrieveVideoByCategory.java
@@ -1,2 +1,23 @@
-package com.freemanpivo.chassi.domain.usecase;public class SearchVideoByCategory {
+package com.freemanpivo.chassi.domain.usecase;
+
+import com.freemanpivo.chassi.domain.model.Video;
+import com.freemanpivo.chassi.domain.port.command.SearchVideoByCategory;
+import com.freemanpivo.chassi.domain.port.operations.GetVideosByCategory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class RetrieveVideoByCategory implements SearchVideoByCategory {
+
+    private final GetVideosByCategory getVideosByCategory;
+
+    @Override
+    public List<Video> getVideos(Long id) {
+        final var videos = getVideosByCategory.getAllVideosByCategory(id);
+
+        if(!videos.isEmpty())
+            return videos;
+        return List.of();
+    }
 }
