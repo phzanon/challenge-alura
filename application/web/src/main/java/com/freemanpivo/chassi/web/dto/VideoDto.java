120ce31edf10f88b623cf7aa0c18506fa362a19d
Ricardoir <40177989+Ricardoir@users.noreply.github.com>
2021-07-27 22:56:02 -0300

    add Category in VideoDto

--- application/web/src/main/java/com/freemanpivo/chassi/web/dto/VideoDto.java
+++ application/web/src/main/java/com/freemanpivo/chassi/web/dto/VideoDto.java
@@ -1,5 +1,7 @@
 package com.freemanpivo.chassi.web.dto;
 
+import com.freemanpivo.chassi.domain.model.Category;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class VideoDto {
-    private String id;
-    private String titulo;
-    private String descricao;
-    private String url;
+	private String id;
+	private String titulo;
+	private String descricao;
+	private String url;
+	private Category category;
 }
