4275bcfe7f13c190bb996b2bb15d9b7df7b28a00
Pedro Zanon <p.hzanon123@gmail.com>
2021-08-01 19:51:45 -0300

    refactor classes

--- core/src/main/java/com/freemanpivo/chassi/domain/port/operations/UpdateVideoModelPort.java
+++
@@ -1,9 +0,0 @@
-package com.freemanpivo.chassi.domain.port.operations;
-
-import com.freemanpivo.chassi.domain.model.Video;
-
-public interface UpdateVideoModelPort {
-
-    Video update(Video video);
-
-}
