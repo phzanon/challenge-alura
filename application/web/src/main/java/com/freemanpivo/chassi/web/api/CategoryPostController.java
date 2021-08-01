9f3a330b49a274d79b51438f3eacbea3772eb805
Pedro Zanon <p.hzanon123@gmail.com>
2021-08-01 11:21:53 -0300

    refactor category post endpoint

--- application/web/src/main/java/com/freemanpivo/chassi/web/api/CategoryPostController.java
+++ application/web/src/main/java/com/freemanpivo/chassi/web/api/CategoryPostController.java
@@ -1,6 +1,10 @@
 package com.freemanpivo.chassi.web.api;
 
-import com.freemanpivo.chassi.web.dto.CategorySaveDto;
+import com.freemanpivo.chassi.domain.model.Category;
+import com.freemanpivo.chassi.domain.port.command.SaveCategoryCommand;
+import com.freemanpivo.chassi.domain.util.CategoryValidator;
+import com.freemanpivo.chassi.web.dto.CategoryPostDto;
+import com.freemanpivo.chassi.web.mappers.CategoryPostDtoMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/categorias")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class CategoryPostController {
 
-    @PostMapping
-    public ResponseEntity<String> save(@RequestBody CategorySaveDto dto) {
+    private final CategoryPostDtoMapper mapper;
+    private final SaveCategoryCommand command;
 
+    @PostMapping("/categorias")
+    public ResponseEntity<Category> save(@RequestBody CategoryPostDto dto) {
+        CategoryValidator.validate(mapper.toModel(dto));
+        Category category = command.save(mapper.toModel(dto));
+        return ResponseEntity.ok(category);
     }
 }
