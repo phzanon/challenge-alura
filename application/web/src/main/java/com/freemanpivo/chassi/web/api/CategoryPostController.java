febf02d8cc3cf3085d9fb47221330843db1ec4fc
Pedro Zanon <p.hzanon123@gmail.com>
2021-08-01 11:40:24 -0300

    update and save categories endpoint

--- application/web/src/main/java/com/freemanpivo/chassi/web/api/CategoryPostController.java
+++ application/web/src/main/java/com/freemanpivo/chassi/web/api/CategoryPostController.java
@@ -3,15 +3,14 @@ package com.freemanpivo.chassi.web.api;
 import com.freemanpivo.chassi.domain.model.Category;
 import com.freemanpivo.chassi.domain.port.command.SaveCategoryCommand;
 import com.freemanpivo.chassi.domain.util.CategoryValidator;
+import com.freemanpivo.chassi.web.dto.CategoryDto;
 import com.freemanpivo.chassi.web.dto.CategoryPostDto;
+import com.freemanpivo.chassi.web.mappers.CategoryDtoMapper;
 import com.freemanpivo.chassi.web.mappers.CategoryPostDtoMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,11 +20,20 @@ public class CategoryPostController {
 
     private final CategoryPostDtoMapper mapper;
     private final SaveCategoryCommand command;
+    private final CategoryDtoMapper dtoMapper;
+    private final SaveCategoryCommand saveCategoryCommand;
 
     @PostMapping("/categorias")
     public ResponseEntity<Category> save(@RequestBody CategoryPostDto dto) {
         CategoryValidator.validate(mapper.toModel(dto));
         Category category = command.save(mapper.toModel(dto));
+        return ResponseEntity.ok(category);
+    }
+
+    @PutMapping
+    public ResponseEntity<Category> updateCategory(@RequestBody CategoryDto dto) {
+        CategoryValidator.validate(dtoMapper.toModel(dto));
+        Category category = saveCategoryCommand.save(dtoMapper.toModel(dto));
         return ResponseEntity.ok(category);
     }
 }
