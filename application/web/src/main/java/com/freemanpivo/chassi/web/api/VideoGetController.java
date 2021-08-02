3492a9e130db255c3e518c4136c9b24bc7a29fad
Ricardo G.I. <ricardo.iannuzzi@hotmail.com>
2021-08-01 23:54:44 -0300

    Creation of search in videoGetcontroller

--- application/web/src/main/java/com/freemanpivo/chassi/web/api/VideoGetController.java
+++ application/web/src/main/java/com/freemanpivo/chassi/web/api/VideoGetController.java
@@ -1,19 +1,20 @@
 package com.freemanpivo.chassi.web.api;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.freemanpivo.chassi.domain.model.Video;
 import com.freemanpivo.chassi.domain.port.command.GetVideosByIdCommand;
+import com.freemanpivo.chassi.domain.port.command.GetVideosByTitleCommand;
 import com.freemanpivo.chassi.domain.port.command.GetVideosCommand;
-import com.freemanpivo.chassi.domain.port.command.SearchVideoByCategory;
-import com.freemanpivo.chassi.domain.util.IdValidator;
-import com.freemanpivo.chassi.web.dto.response.ResponseGetAll;
-import com.freemanpivo.chassi.web.mappers.VideoGetDtoMapper;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/videos")
@@ -23,6 +24,7 @@ public class VideoGetController {
 
     private final GetVideosCommand command;
     private final GetVideosByIdCommand getVideosByIdCommand;
+    private final GetVideosByTitleCommand getVideosByTitleCommand;
 
     @GetMapping("/all")
     public ResponseEntity<List<Video>> getAll() {
@@ -32,5 +34,10 @@ public class VideoGetController {
     @GetMapping
     public ResponseEntity<Video> getById(@RequestParam("id") String id) {
         return ResponseEntity.ok(getVideosByIdCommand.findVideosById(Long.parseLong(id)));
+    }
+    
+    @GetMapping("/search")
+    public ResponseEntity<Video> getByTitle(@RequestParam("titulo") String title) {
+		return ResponseEntity.ok(getVideosByTitleCommand.findVideosByTitle(title));
     }
 }
